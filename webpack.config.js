ebce121f6d9dbf9acc3ce007f4d1da4f03899bf9
sarkiroka <sarki_roka@freemail.hu>
2016-10-28 22:21:10 +0200

    the config of the webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/**
+ * @author sarkiroka
+ */
+var webpack = require('webpack');
+module.exports = {
+	entry: './client/src/j/robotos.js',
+	module: {
+		loaders: [
+			{
+				test:/.*\.js$/,
+				loader: 'babel?presets[]=es2015'
+			}
+		]
+	},
+	output: {
+		filename: 'r.js',
+		path: './client/static/j'
+	},
+	plugins: [new webpack.optimize.UglifyJsPlugin({})]
+};

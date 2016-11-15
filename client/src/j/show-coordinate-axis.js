eac03e44071db24e3ebe2b74e201ab68e94d2d3c
sarkiroka <sarki_roka@freemail.hu>
2016-11-15 22:04:28 +0100

    use constant for size of axis

--- client/src/j/show-coordinate-axis.js
+++ client/src/j/show-coordinate-axis.js
@@ -1,8 +1,9 @@
 /**
  * @author sarkiroka on 2016.11.13.
  */
+const SIZE_OF_AXIS = 50;
 module.exports = function (scene) {
 	//red:x, green:y, blue:z
-	var axisHelper = new THREE.AxisHelper(50);
+	var axisHelper = new THREE.AxisHelper(SIZE_OF_AXIS);
 	scene.add(axisHelper);
 };

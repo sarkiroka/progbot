e1821958c2698fcc2825fe2603c8f9c1cd5d437c
sarkiroka <sarki_roka@freemail.hu>
2016-11-27 01:37:14 +0100

    use the new keyboard based codes

--- client/src/j/init.js
+++ client/src/j/init.js
@@ -1,12 +1,16 @@
 /**
  * @author sarkiroka
  */
+var handleKeyboard = require('./handle-keyboard');
 var resizeWindow = require('./resize-window');
+var setupNewCommand = require('./setup-new-command');
+var keyboardHandlers = require('./keyboard-handlers');
 module.exports = function () {
 	var scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	var renderer = new THREE.WebGLRenderer({antialias: true});
 	resizeWindow(scene, camera, renderer);
+	handleKeyboard(setupNewCommand(keyboardHandlers));
 	document.body.appendChild(renderer.domElement);
 	return {scene, camera, renderer};
 };

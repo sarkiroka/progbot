be945d22425323bfc6f8026c093e48f045fd78de
sarkiroka <sarki_roka@freemail.hu>
2016-12-03 23:54:46 +0100

    change to a better name the input dispatcher... hmm... is this a more better name?

--- client/src/j/init.js
+++ client/src/j/init.js
@@ -3,14 +3,14 @@
  */
 var handleKeyboard = require('./handle-keyboard');
 var resizeWindow = require('./resize-window');
-var setupNewCommand = require('./setup-new-command');
+var setupKeyboardCommands = require('./setup-keyboard-command');
 var keyboardHandlers = require('./keyboard-handlers');
 module.exports = function () {
 	var scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	var renderer = new THREE.WebGLRenderer({antialias: true});
 	resizeWindow(scene, camera, renderer);
-	handleKeyboard(setupNewCommand(keyboardHandlers));
+	handleKeyboard(setupKeyboardCommands(keyboardHandlers));
 	document.body.appendChild(renderer.domElement);
 	return {scene, camera, renderer};
 };

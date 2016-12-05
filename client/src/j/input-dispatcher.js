3585546c81d82c474d0a1707132f859f1436791c
sarkiroka <sarki_roka@freemail.hu>
2016-12-06 00:41:42 +0100

    rename the variable, it is not a config but the dispatcher

--- client/src/j/input-dispatcher.js
+++ client/src/j/input-dispatcher.js
@@ -2,21 +2,21 @@
  * @author sarkiroka on 2016.12.25.
  */
 var constants = require('./constants');
-module.exports = function (config) {
+module.exports = function (dispatcher) {
 	return function (code) {
 		switch (code) {
 			case 'Enter':
 			case 'NumpadEnter':
-				config.start();
+				dispatcher.start();
 				break;
 			case 'ArrowUp':
-				config.move(constants.DIRECTIONS.FORWARD);
+				dispatcher.move(constants.DIRECTIONS.FORWARD);
 				break;
 			case 'ArrowRight':
-				config.move(constants.DIRECTIONS.RIGHT);
+				dispatcher.move(constants.DIRECTIONS.RIGHT);
 				break;
 			case 'ArrowLeft':
-				config.move(constants.DIRECTIONS.LEFT);
+				dispatcher.move(constants.DIRECTIONS.LEFT);
 				break;
 			default:
 				console.warn('unhandled keyboard event', code);

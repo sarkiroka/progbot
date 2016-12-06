d0808387a131e1a5b744f554885343fe0ceace84
sarkiroka <sarki_roka@freemail.hu>
2016-12-07 00:47:17 +0100

    rename the keyboard dispatcher

--- client/src/j/init.js
+++ client/src/j/init.js
@@ -4,13 +4,13 @@
 var handleKeyboard = require('./handle-keyboard');
 var resizeWindow = require('./resize-window');
 var inputDispatcher = require('./input-dispatcher');
-var keyboardHandlers = require('./keyboard-handlers');
+var keyboardDispatcher = require('./keyboard-dispatcher');
 module.exports = function () {
 	var scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	var renderer = new THREE.WebGLRenderer({antialias: true});
 	resizeWindow(scene, camera, renderer);
-	handleKeyboard(inputDispatcher(keyboardHandlers));
+	handleKeyboard(inputDispatcher(keyboardDispatcher));
 	document.body.appendChild(renderer.domElement);
 	return {scene, camera, renderer};
 };

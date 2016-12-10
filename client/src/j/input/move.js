4b2371e54af91da0df3c43fa409ab29d6b562591
sarkiroka <sarki_roka@freemail.hu>
2016-12-11 00:54:04 +0100

    the move module

---
+++ client/src/j/input/move.js
@@ -0,0 +1,8 @@
+/**
+ * handles the programming of a move
+ * @author sarkiroka on 2016.12.11.
+ */
+var stateOfTheGame=require('../state-of-the-game');
+module.exports = function (direction) {
+	console.log(direction);
+};

15da1d5f0136e61e04e2fad108dea5b44067b8eb
sarkiroka <sarki_roka@freemail.hu>
2016-12-17 01:16:15 +0100

    handle the enter key

---
+++ client/src/j/input/start.js
@@ -0,0 +1,17 @@
+/**
+ * handle the enter key
+ * @author sarkiroka on 2016.12.17.
+ */
+var constants = require('../constants');
+var stateOfTheGame = require('../state-of-the-game');
+module.exports = function () {
+	if (stateOfTheGame.is == constants.GAME_STATE.PROGRAMMING) {
+		stateOfTheGame.is = constants.GAME_STATE.RUNNING;
+		setTimeout(()=> {
+			stateOfTheGame.is = constants.GAME_STATE.PROGRAMMING;
+			console.info('returning to the programming state');
+		}, 5000);
+	} else {
+		console.warn('already in running/result');
+	}
+};

f3565f7a221aeda9296bcc1437ad9dc447e7641f
sarkiroka <sarki_roka@freemail.hu>
2016-12-20 00:31:08 +0100

    the game is only a holder

--- client/src/j/game/the-game.js
+++ client/src/j/game/the-game.js
@@ -2,7 +2,8 @@
  * holds the state of the game. it is readable, or writeable for every module
  * @author sarkiroka on 2016.11.28.
  */
-var constants = require('./constants');
+var constants = require('./../constants');
 module.exports = {
-	is: constants.GAME_STATE.PROGRAMMING
+	is: constants.GAME_STATE.PROGRAMMING,
+	program: []
 };

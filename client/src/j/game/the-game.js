7aef49de054f3580f6ee59a93f57ceeeb472beb6
sarkiroka <sarki_roka@freemail.hu>
2016-12-24 00:49:04 +0100

    some instruction ordering

--- client/src/j/game/the-game.js
+++ client/src/j/game/the-game.js
@@ -2,7 +2,8 @@
  * holds the state of the game. it is readable, or writeable for every module
  * @author sarkiroka on 2016.11.28.
  */
-var reset = require('./reset');
+var theGame = {};
+module.exports = theGame;
 
+var reset = require('./reset');
 reset();
-module.exports = {};

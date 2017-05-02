f29dac9c9872fbdae068186467107ae76d5c4890
sarkiroka <sarki_roka@freemail.hu>
2017-05-02 20:55:16 +0200

    webpack... :( why must create configuration files in all time?

--- developer-tools/webpack.config.js
+++ developer-tools/webpack.config.js
@@ -1,21 +1,35 @@
 /**
- * @author sarkiroka
+ *
+ * @author sarkiroka on 2017.05.02.
  */
-var webpack = require('webpack');
+const endOfBuild = require('./end-of-build')('JS');
+const packageJSON = require('./package.json');
+const path = require('path');
+const webpack = require('webpack');
+const WebpackOnBuildPlugin = require('on-build-webpack');
+
+const PROJECT_BASE = path.normalize(path.join(__dirname, '..'));
+const ENTRY_PATH = path.join(PROJECT_BASE, 'source', 'index.js');
+const TARGET_PATH = path.join(PROJECT_BASE, 'dist');
+
 module.exports = {
-	entry: './client/src/j/main-part.js',
-	module: {
-		rules: [
-			{
-				test:/.*\.js$/,
-				use: 'babel-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
+	entry: ENTRY_PATH,
 	output: {
-		filename: 'r.js',
-		path: './client/static/j'
+		path: TARGET_PATH,
+		filename: 'miniprog.js'
 	},
-	plugins: [/*new webpack.optimize.UglifyJsPlugin({})*/]
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {},
+			output: {
+				comments: false
+			}
+		}),
+		new webpack.BannerPlugin({
+			banner: `${packageJSON.name} v${packageJSON.version} (c) ${packageJSON.author}`,
+			entryOnly: true
+		}),
+		new WebpackOnBuildPlugin(endOfBuild)
+	],
+	stats: 'errors-only'
 };

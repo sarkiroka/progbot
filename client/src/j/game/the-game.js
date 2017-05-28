561bc5ba2fa82230b67b5e6ce1a4339fd2671748
sarkiroka <sarki_roka@freemail.hu>
2017-05-28 21:09:21 +0200

    kill legacy code

--- client/src/j/game/the-game.js
+++
@@ -1,9 +0,0 @@
-/**
- * holds the state of the game. it is readable, or writeable for every module
- * @author sarkiroka on 2016.11.28.
- */
-var theGame = {};
-module.exports = theGame;
-
-var reset = require('./reset');
-reset();

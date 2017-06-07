e71361a763355a4f4927abf5a76b3e023089cc20
sarkiroka <sarki_roka@freemail.hu>
2017-06-07 21:50:37 +0200

    setup debug in client side - why is complex? :(

---
+++ source/init/debug.js
@@ -0,0 +1,8 @@
+/**
+ * debug initialize in browser
+ * @author sarkiroka on 2017.12.01.
+ */
+var Debug = require('debug');
+Debug.enable('progbot:*');
+var debug = Debug('progbot:init:debug');
+debug('start');

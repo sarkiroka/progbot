5578ad25a9322f094b50f3fcf81874f391eb50df
sarkiroka <sarki_roka@freemail.hu>
2017-06-08 21:51:36 +0200

    add a store for drawing objects

---
+++ source/common/draw/canvas.js
@@ -0,0 +1,15 @@
+/**
+ * ez tárolja a canvas-os objektumokat
+ * @author sarkiroka on 2017.12.01.
+ */
+var exportObject = function (canvas, ctx) {
+	exportObject.canvas = canvas;
+	exportObject.ctx = ctx;
+};
+exportObject.canvas = null;
+exportObject.ctx = null;
+exportObject.setSize = function (width, height) {
+	exportObject.canvas.width = width;
+	exportObject.canvas.height = height;
+};
+module.exports = exportObject;

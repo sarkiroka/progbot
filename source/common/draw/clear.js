c9b4c418f6426127df7f3d54fb7eb82ad119b138
sarkiroka <sarki_roka@freemail.hu>
2017-06-09 21:51:57 +0200

    clear method

---
+++ source/common/draw/clear.js
@@ -0,0 +1,13 @@
+/**
+ * törli a transzformációs mátrixot és lesatírozza a képernyőt
+ * @author sarkiroka on 2017.12.02.
+ */
+var canvas = require('./canvas');
+var debug = require('debug')('progbot:draw:clear');
+module.exports = function () {
+	debug('clear all pixels');
+	var ctx = canvas.ctx;
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
+	ctx.fillStyle = '#aaa';
+	ctx.fillRect(0, 0, canvas.canvas.width, canvas.canvas.height);
+};

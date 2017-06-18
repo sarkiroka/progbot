0c77825125fecc533cff3d3fd8c172865cedeff5
sarkiroka <sarki_roka@freemail.hu>
2017-06-18 21:57:30 +0200

    on keypress handler

---
+++ source/common/event/on-key-event.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * @author sarkiroka on 2017.06.01.
+ */
+var onEndOfGame=require('./on/end-of-game');
+module.exports = function (event) {
+	event.preventDefault();
+	switch (event.key) {
+		case 'Escape':
+			onEndOfGame();
+			break;
+		case 'Enter':
+			drawCurrentPosition();
+			checkResult(function (success) {
+				if (success) {
+					currentLevel++;
+					win();
+					reDraw();
+				} else {
+					loose();
+				}
+				setDefaults();
+			});
+			break;
+		case 'ArrowLeft':
+		case 'ArrowRight':
+		case 'ArrowUp':
+		case 'ArrowDown':
+			program.push(event.key.replace('Arrow', '').toLowerCase());
+			reDraw();
+			break;
+		case 'Backspace':
+			program.pop();
+			reDraw();
+			break;
+		case 'F5':
+			location.reload();
+			break;
+		default:
+			console.log('unhandled keyboard event', event);
+	}
+};

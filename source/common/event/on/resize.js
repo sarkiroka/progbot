04b63586dcf6f06d9ddd1d8c94deb491f21b2e4e
sarkiroka <sarki_roka@freemail.hu>
2017-06-20 21:58:24 +0200

    resize handler

---
+++ source/common/event/on/resize.js
@@ -0,0 +1,10 @@
+/**
+ * az átméretezés kezelője
+ * @author sarkiroka on 2017.06.02.
+ */
+var canvas = require('../../draw/canvas');
+var debug = require('debug')('progbot:event:resize');
+module.exports = function () {
+	debug('resize event handling');
+	canvas.setSize(window.innerWidth, window.innerHeight)
+};

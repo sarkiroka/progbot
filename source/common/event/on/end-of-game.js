205009c097ba237d7c774591da6cbe9ed88154ec
sarkiroka <sarki_roka@freemail.hu>
2017-07-17 22:22:30 +0200

    code formatting

--- source/common/event/on/end-of-game.js
+++ source/common/event/on/end-of-game.js
@@ -2,11 +2,11 @@
  * the end
  * @author sarkiroka on 2017.06.02.
  */
-var clear=require('../../draw/clear');
-var debug=require('debug')('progbot:event:end-of-game');
-var theEnd=require('../../draw/the-end');
+var clear = require('../../draw/clear');
+var debug = require('debug')('progbot:event:end-of-game');
+var theEnd = require('../../draw/the-end');
 module.exports = function () {
-	var initKeyboard=require('../../../init/init-keyboard');
+	var initKeyboard = require('../../../init/init-keyboard');
 	debug('end of the game');
 	initKeyboard.done();
 	clear();

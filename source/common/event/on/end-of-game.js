e55ae4bb56672726fd94b3eb78acb37e3e2a5f7e
sarkiroka <sarki_roka@freemail.hu>
2017-07-20 22:25:55 +0200

    end

--- source/common/event/on/end-of-game.js
+++ source/common/event/on/end-of-game.js
@@ -2,7 +2,6 @@
  * the end
  * @author sarkiroka on 2017.06.02.
  */
-var clear = require('../../draw/clear');
 var debug = require('debug')('progbot:event:end-of-game');
 var theEnd = require('../../draw/the-end');
 module.exports = function () {

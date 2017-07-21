f267ac957eadf6e16cf22aa1d085fa8759650bb7
sarkiroka <sarki_roka@freemail.hu>
2017-07-21 22:26:37 +0200

    explanation

--- source/common/event/on/end-of-game.js
+++ source/common/event/on/end-of-game.js
@@ -5,7 +5,7 @@
 var debug = require('debug')('progbot:event:end-of-game');
 var theEnd = require('../../draw/the-end');
 module.exports = function () {
-	var initKeyboard = require('../../../init/init-keyboard');
+	var initKeyboard = require('../../../init/init-keyboard');// because circular dependency
 	debug('end of the game');
 	initKeyboard.done();
 	theEnd();

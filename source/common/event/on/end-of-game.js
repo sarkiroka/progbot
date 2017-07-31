d7371cbdb677d52a90e95eef08f909c4e0344007
sarkiroka <sarki_roka@freemail.hu>
2017-07-31 22:32:24 +0200

    end

--- source/common/event/on/end-of-game.js
+++ source/common/event/on/end-of-game.js
@@ -3,10 +3,10 @@
  * @author sarkiroka on 2017.06.02.
  */
 var debug = require('debug')('progbot:event:end-of-game');
-var theEnd = require('../../draw/the-end');
+var drawTheEnd = require('../../draw/the-end');
 module.exports = function () {
 	var initKeyboard = require('../../../init/init-keyboard');// because circular dependency
 	debug('end of the game');
 	initKeyboard.done();
-	theEnd();
+	drawTheEnd();
 };

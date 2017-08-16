79b13b98c665dd22c84fd5b27e318adb95c4e0f3
sarkiroka <sarki_roka@freemail.hu>
2017-08-16 22:58:06 +0200

    autoformat code

--- source/common/event/on/backspace.js
+++ source/common/event/on/backspace.js
@@ -2,7 +2,7 @@
  * handle of the backspace button
  * @author sarkiroka on 2017.08.10.
  */
-var redraw=require('../../draw/redraw');
+var redraw = require('../../draw/redraw');
 module.exports = function () {
 
 };

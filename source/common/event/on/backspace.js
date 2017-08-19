bf0d24e76292d04fc8f6b9ec37ab0d2a01e3552b
sarkiroka <sarki_roka@freemail.hu>
2017-08-19 23:02:34 +0200

    connect the game and the event handler

--- source/common/event/on/backspace.js
+++ source/common/event/on/backspace.js
@@ -2,6 +2,7 @@
  * handle of the backspace button
  * @author sarkiroka on 2017.08.10.
  */
+var gameOnBackspace=require('../../../game/on/backspace');
 var redraw = require('../../draw/redraw');
 module.exports = function () {
 	redraw();
